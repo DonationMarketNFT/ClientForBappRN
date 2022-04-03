d27adae90098a8603286b38c8f517bb3e029b20f
downy <TeamBehindDowny@gmail.com>
2022-04-03 15:32:57 +0900

    add WebView

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,13 @@
 import react from "react";
-import { View, Text, FlatList, ScrollView} from 'react-native';
+import { View, Text, FlatList, ScrollView } from "react-native";
+import { WebView } from "react-native-webview";
 import { RenderCampaign } from "./components/RenderCampaign";
 
-const HomeScreen = () =>{
-    const renderBanner = () =>(
-        <View style={{flex:1, height:200, margin:16, borderRadius:16, backgroundColor:'white'}}>
-            <Text style={{margin:8, fontSize:20}}>배너</Text>
-        </View>
-    )
-
-    return(
-        <ScrollView>
-            <View style={{marginTop:20}}/>
-            {renderBanner()}
-            <View style={{marginTop:20}}/>
-            <RenderCampaign campaignName='New Campaign' data={[1,2,3,4]}/>
-            <View style={{marginTop:20}}/>
-            <RenderCampaign campaignName='Popular Campaign' data={[1,2,3,4]} color='blue'/>
-            <View style={{marginTop:20}}/>
-            <RenderCampaign campaignName='My Campaign' data={[1,2,3,4]} color='white'/>
-            <View style={{marginTop:20}}/>
-        </ScrollView>
-    )
+const HomeScreen = () => {
+  return (
+    <View style={{ width: "100%", height: "100%", backgroundColor: "red" }}>
+      <WebView source={{ uri: "http://localhost:3003" }}></WebView>
+    </View>
+  );
 };
 export default HomeScreen;
