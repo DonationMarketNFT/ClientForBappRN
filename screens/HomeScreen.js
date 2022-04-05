a0cb59ae2de5360131f4b5920e7b13c0c20ba5cd
downy <TeamBehindDowny@gmail.com>
2022-04-06 01:08:27 +0900

    SafeAreaView 삭제

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,10 +8,9 @@ import { StatusBar } from "expo-status-bar";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={{ width: "100%", height: "100%" }}>
-      <StatusBar barStyle="light-content"></StatusBar>
+    <View style={{ width: "100%", height: "100%" }}>
       <WebView source={{ uri: "http://localhost:3003" }}></WebView>
-    </SafeAreaView>
+    </View>
   );
 };
 export default HomeScreen;
